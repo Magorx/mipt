892a147aa47d26948a8cca3d76789b0792e68750
MaximusTG <maxim.gorishniy@gmail.com>
2020-09-09 00:31:26 +0300

    refactored

--- seminars/ded/onegin/onegin.c
+++ seminars/ded/onegin/onegin.c
@@ -4,7 +4,50 @@
 #include <stdlib.h>
 
 const int MAXSTRLEN = 100;
-const int MAXSTRS = 10000;
+const int MAXSTRS   = 10000;
+
+const char ERROR = -1;
+
+int compare_strings(const void **elem1, const void **elem2);
+int reverse_compare_strings(const void **elem1, const void **elem2);
+void free_lines_memory(unsigned char **lines);
+int read_lines(char *file_name, unsigned char **lines);
+int print_lines(char *file_names, unsigned char **lines, int lines_cnt);
+
+int main(const int argc, const char **argv) {
+    setlocale(LC_ALL,"Russian");
+
+    unsigned char **lines = calloc(MAXSTRS, sizeof(char*));
+    unsigned char **lines_ptr = lines;
+    for (int i = 0; i < MAXSTRS; ++i) {
+        *lines_ptr = calloc(MAXSTRLEN, sizeof(char*));
+        ++lines_ptr;
+    }
+
+    char *fin_name = "onegin.txt";
+    char *fout_name = "oneginized.txt";
+    if (argc > 1) {
+        fin_name = argv[1];
+    }
+    if (argc > 2) {
+        fout_name = argv[2];
+    }
+
+    int lines_cnt = read_lines(fin_name, lines);
+    if (lines_cnt == ERROR) {
+        return 0;
+    }
+
+    qsort(lines, lines_cnt, sizeof(char*), compare_strings);
+    printf("%d lines are sorted!\n", lines_cnt);
+
+    print_lines(fout_name, lines, lines_cnt);
+
+    free_lines_memory(lines);
+
+    return 0;
+
+}
 
 char is_russian_letter(unsigned char c) {
     return (c >= (unsigned char) 'À' && c <= (unsigned char) 'ß') || (c >= (unsigned char) 'à' && c <= (unsigned char) 'ÿ');
@@ -42,10 +85,6 @@ int reverse_compare_strings(const void **elem1, const void **elem2) {
     return -compare_strings(elem1, elem2);
 }
 
-void char_test(unsigned char x) {
-    printf("%c %d\n", x, x);
-}
-
 void free_lines_memory(unsigned char **lines) {
     unsigned char **lines_ptr = lines;
     for (int i = 0; i < MAXSTRS; ++i) {
@@ -55,56 +94,33 @@ void free_lines_memory(unsigned char **lines) {
     free(lines);
 }
 
-int main(const int argc, const char **argv) {
-    setlocale(LC_ALL,"Russian");
-
-    unsigned char **lines = calloc(MAXSTRS, sizeof(char*));
-    unsigned char **lines_ptr = lines;
-    for (int i = 0; i < MAXSTRS; ++i) {
-        *lines_ptr = calloc(MAXSTRLEN, sizeof(char*));
-        ++lines_ptr;
-    }
-
-    char *fin_name = "onegin.txt";
-    char *fout_name = "oneginized.txt";
-    if (argc > 1) {
-        fin_name = argv[1];
-    }
-    if (argc > 2) {
-        fout_name = argv[2];
-    }
-
-    FILE *fin = fopen(fin_name, "r");
+int read_lines(char *file_name, unsigned char **lines) {
+    FILE *fin = fopen(file_name, "r");
     if (!fin) {
-        printf("File '%s' not found!\n", fin_name);
+        printf("File '%s' not found!\n", file_name);
         free_lines_memory(lines);
-        return 0;
+        return ERROR;
     }
 
-    lines_ptr = lines;
-    int str_cnt = 0;
-    printf("%d\n", fin);
+    unsigned char **lines_ptr = lines;
+    int lines_cnt = 0;
     while (fgets(*(lines_ptr++), sizeof(char) * MAXSTRLEN, fin)) {
-        str_cnt += 1;
-        if (str_cnt == MAXSTRS - 1) {
+        lines_cnt += 1;
+        if (lines_cnt == MAXSTRS - 1) {
             printf("Can't handle such a big file!\n");
             free_lines_memory(lines);
-            return 0;
+            return ERROR;
         }
     }
 
-    qsort(lines, str_cnt, sizeof(char*), compare_strings);
-    printf("%d lines are sorted!\n", str_cnt);
+    return lines_cnt;
+}
 
-    FILE *fout = fopen(fout_name, "w");
-    for (int i = 0; i < str_cnt; ++i) {
+int print_lines(char *file_name, unsigned char **lines, int lines_cnt) {
+    FILE *fout = fopen(file_name, "w");
+    for (int i = 0; i < lines_cnt; ++i) {
         fprintf(fout, "%s", lines[i]);
     }
 
     fclose(fout);
-    free_lines_memory(lines);
-
-    return 0;
-
 }
- ;
