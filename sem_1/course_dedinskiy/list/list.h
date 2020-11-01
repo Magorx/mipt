8064f94d1f35eae0a427c3cb0b29902788b79273
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2020-11-02 01:27:21 +0300

    added linear search

--- sem_1/course_dedinskiy/list/list.h
+++ sem_1/course_dedinskiy/list/list.h
@@ -15,6 +15,7 @@ typedef struct Node_t {
 
 typedef struct List_t {
 	Node *buffer;
+	int fictive_node;
 
 	size_t capacity;
 	size_t size;
@@ -43,6 +44,14 @@ int List_push_back();
 //=============================================================================
 // Implementation =============================================================
 
+int List_head(const List *cake) {
+	return cake->buffer[cake->fictive_node].next;
+}
+
+int List_tail(const List *cake) {
+	return cake->buffer[cake->fictive_node].prev;
+}
+
 int List_valid(const List *cake) {
 	return OK;
 	if (!cake) {
@@ -55,13 +64,15 @@ int List_valid(const List *cake) {
 
 	{
 		char *visited = (char*) calloc(cake->capacity, sizeof(char));
-		for (int i = cake->head; (int) i != cake->tail; i = cake->buffer[i].next) {
+		int head = List_head(cake);
+		int tail = List_tail(cake);
+		for (int i = head; (int) i != tail; i = cake->buffer[i].next) {
 			if (visited[i]) {
 				RETURNING_VERIFY(ERROR_UNEXPECTED_LOOP);
 			}
 			visited[i] = 1;
 		}
-		visited[cake->tail] = 1;
+		visited[tail] = 1;
 		int visited_sum = 0;
 		for (size_t i = 0; i < cake->capacity; ++i) {
 			visited_sum += (int) visited[i];
@@ -91,15 +102,14 @@ List *new_List() {
 
 	l->capacity = cap;
 	l->size = 0;
-	l->head = 1;
-	l->tail = 1;
-	l->free_head = l->head;
+	l->fictive_node = 0;
+	l->free_head = 1;
 
-	for (size_t i = 1; i < cap; ++i) {
+	for (size_t i = 0; i < cap; ++i) {
 		l->buffer[i].next = (int) i + 1;
-		// printf("%d\n", i);
 		l->buffer[i].prev = 0;
 	}
+	l->buffer[0].next = 0;
 
 	VERIFY_T(List_valid(l) == OK, List*);
 	return l;
@@ -111,8 +121,6 @@ int delete_List(List *cake) {
 ;	free(cake->buffer);
 	
 	cake->capacity  = (size_t) KCTF_POISON;
-	cake->head      = (int) KCTF_POISON;
-	cake->tail      = (int) KCTF_POISON;
 
 	free(cake);
 
@@ -122,10 +130,12 @@ int delete_List(List *cake) {
 int List_dump(const List *cake) {
 	VERIFY_OK(List_valid(cake));
 
-	for (int i = cake->head; i != cake->tail; i = cake->buffer[i].next) {
-		printf("%d -> ", cake->buffer[i].data);
+	int head = List_head(cake);
+	int tail = List_tail(cake);
+	for (int i = head; i != tail; i = cake->buffer[i].next) {
+		printf("[%d](%d) -> ", i, cake->buffer[i].data);
 	}
-	printf("%d -> ", cake->buffer[cake->tail].data);
+	printf("[%d](%d)", tail, cake->buffer[tail].data);
 	printf("\n");
 
 	return 0;
@@ -159,22 +169,14 @@ int List_set_capacity(List *cake, const size_t capacity) {
 
 int List_set_node_value(List *cake, const int node, const int left_node, const int right_node, const LIST_TYPE *data) {
 	cake->buffer[node].data = *data;
-	cake->buffer[node].next = node;
-	cake->buffer[node].prev = node;
+	cake->buffer[node].next =  node;
+	cake->buffer[node].prev =  node;
 
-	if (left_node) {
-		cake->buffer[left_node].next = node;
-		cake->buffer[node].prev  = left_node;
-	} else {
-		cake->head = node;
-	}
+	cake->buffer[left_node].next = node;
+	cake->buffer[node].prev = left_node;
 
-	if (right_node) {
-		cake->buffer[right_node].prev = node;
-		cake->buffer[node].next  = right_node;
-	} else {
-		cake->tail = node;
-	}
+	cake->buffer[right_node].prev = node;
+	cake->buffer[node].next = right_node;
 
 	return 0;
 }
@@ -192,9 +194,11 @@ int List_push_right(List *cake, int node, LIST_TYPE data) {
 
 	cake->free_head = cake->buffer[next_free].next;
 
-	int next_node = cake->buffer[node].next * (node != cake->tail);
+	int next_node = cake->buffer[node].next;
 	List_set_node_value(cake, next_free, node, next_node, &data);
+	
 	cake->size += 1;
+	cake->max_sorted_index = node < cake->max_sorted_index ? node : cake->max_sorted_index;
 
 	return 0;
 }
@@ -212,19 +216,21 @@ int List_push_left(List *cake, int node, LIST_TYPE data) {
 
 	cake->free_head = cake->buffer[next_free].next;
 
-	int prev_node = cake->buffer[node].prev * (node != cake->head);
+	int prev_node = cake->buffer[node].prev;
 	List_set_node_value(cake, next_free, prev_node, node, &data);
 	cake->size += 1;
+
+	cake->max_sorted_index = node - 1 < cake->max_sorted_index ? node - 1 : cake->max_sorted_index;
 
 	return 0;
 }
 
 int List_push_front(List *cake, LIST_TYPE data) {
-	return List_push_left(cake, cake->head, data);
+	return List_push_left(cake, cake->fictive_node, data);
 }
 
 int List_push_back(List *cake, LIST_TYPE data) {
-	return List_push_right(cake, cake->tail, data);
+	return List_push_right(cake, cake->fictive_node, data);
 }
 
 int List_pop(List *cake, const int node) {
@@ -234,41 +240,53 @@ int List_pop(List *cake, const int node) {
 	int next_node = cake->buffer[node].next;
 	int prev_node = cake->buffer[node].prev;
 
-	if (node == cake->head) {
-		cake->head = next_node;
-	}
-	if (node == cake->tail) {
-		cake->tail = prev_node;
-	}
-
 	cake->buffer[node].next = prev_node;
 	cake->buffer[node].prev = next_node;
 
-	//cake->buffer[node].data = (LIST_TYPE) KCTF_POISON;
 	--cake->size;
+	cake->max_sorted_index = node - 1 < cake->max_sorted_index ? node - 1 : cake->max_sorted_index;
 
 	return 0;
 }
 
-int List_sort(List *cake) {
+int List_linear_optimization(List *cake) {
 	VERIFY_OK(List_valid(cake));
 
 	Node *new_buffer = (Node*) calloc(cake->capacity, sizeof(Node));
-	int node_index = cake->head;
+	int node_index = cake->fictive_node;
 	int inted_size = (int) cake->size;
-	for (int i = 1; i <= inted_size; ++i) {
+	for (int i = 0; i <= inted_size; ++i) {
 		new_buffer[i].data = cake->buffer[node_index].data;
 		new_buffer[i].next = i + 1;
 		new_buffer[i].prev = i - 1;
 		node_index = cake->buffer[node_index].next;
 	}
+	new_buffer[cake->size].next = 0;
+	new_buffer[0].prev = (int) cake->size;
 	free(cake->buffer);
 
-	cake->head = 1;
-	cake->tail = inted_size;
 	cake->buffer = new_buffer;
 
 	cake->max_sorted_index = inted_size;
 
 	return 0;
+}
+
+int List_linear_index_search(const List *cake, int index) {
+	VERIFY_OK(List_valid(cake));
+	++index;
+	if (index < 1) {
+		RETURNING_VERIFY(ERROR_BAD_ARGS);
+	}
+
+	printf("%d vs %d\n", index, cake->max_sorted_index);
+	if (index <= cake->max_sorted_index) {
+		return index;
+	} else {
+		int node = List_head(cake);
+		for (int i = 1; i < index; ++i) {
+			node = cake->buffer[node].next;
+		}
+		return node;
+	}
 }
--- sem_1/course_dedinskiy/list/main.c
+++ sem_1/course_dedinskiy/list/main.c
@@ -4,18 +4,20 @@
 #include "list.h"
 #undef LIST_TYPE
 
-#define TIME_MEASURED(code){clock_t begin = clock();code;clock_t end = clock();\
+#define TIME_MEASURED(code){clock_t begin = clock();{code};clock_t end = clock();\
 double elapsed_secs = (double)(end - begin) / CLOCKS_PER_SEC;\
 printf("ELAPSED: %lf sec\n", elapsed_secs);}
 
 int main() {
+	srand((unsigned int) time(NULL));
 	List *l = new_List();
+	int N = 100;
 	
 	printf("Pushing\n");
-	TIME_MEASURED({
 
-	List_push_right(l, 1, -1);
-	for (int i = 1; i <= 100000000; ++i) {
+	TIME_MEASURED(
+
+	for (int i = 1; i <= N; ++i) {
 		if (rand() % 2) {
 			List_push_front(l, i);
 		} else {
@@ -26,15 +28,16 @@ int main() {
 		}
 	}
 
-	});
-
-	printf("Sorting\n");
-	TIME_MEASURED({
-	List_sort(l);
-	});
+	);
 
 	//List_dump(l);
 
+	printf("Sorting\n");
+	TIME_MEASURED(
+
+	List_linear_optimization(l);
+
+	);
 	delete_List(l);
 
 	printf("done.\n");
