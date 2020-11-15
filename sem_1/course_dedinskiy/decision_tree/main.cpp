8e838d7eb01d3d52ec50dbf00e84dad2eb3e4799
KingCakeTheFruity <maxim.gorishniy@gmail.com>
2020-11-15 03:24:06 +0300

    working!

--- sem_1/course_dedinskiy/decision_tree/main.cpp
+++ sem_1/course_dedinskiy/decision_tree/main.cpp
@@ -2,13 +2,20 @@
 #include <cstdio>
 
 int main() {
+	/* merge 
 	DecisionTree first, second;
 	first.load("db1.db");
 	second.load("db2.db");
 
 	first.merge(second);
 
-	first.save("db_out.db");
+	first.save("db_out.db");*/
+
+	DecisionTree tree;
+	tree.load("db.db");
+
+	tree.run_guess();
+	tree.save("db_out.db");
 
 	printf(".doned.\n");
 	return 0;
